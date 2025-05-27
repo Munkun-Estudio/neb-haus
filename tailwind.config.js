0f0ca01b9f0424d4f625c6a0ffec35e6cda5c26e
Mateusz <lukasiewicz.matteusz@gmail.com>
2025-05-27 12:58:35 +0200

    We clear website

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,erb,md}",
+    "./frontend/javascript/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
